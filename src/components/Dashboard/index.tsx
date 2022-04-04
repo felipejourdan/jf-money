498ea274c9fe8297e04af2d87e27ad05808d6d6c
felipejourdan <fjourdanp@gmail.com>
2022-04-04 16:03:41 +0100

    some file adjust

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Summary } from '../Summury'
+import { Summary } from '../Summary'
 import { TransactionTable } from '../TransactionTable'
 import { Container } from './styles'
 

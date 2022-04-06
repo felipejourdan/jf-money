79f09578fe34c9ee1626bb54f94d187c9d546274
felipejourdan <fjourdanp@gmail.com>
2022-04-06 18:15:32 +0100

    conection with fake  database/api using mirage

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -2,9 +2,22 @@ import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 import { Container } from './styles'
 
+interface transactionProps {
+  id: number
+  title: string
+  type: string
+  category: string
+  amount: number
+  atDate: string
+}
+
 export const TransactionTable = () => {
+  const [transactions, setTransactions] = useState<transactionProps[]>([])
+
   useEffect(() => {
-    api.get('transactions').then(response => console.log(response.data))
+    api
+      .get('transactions')
+      .then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -20,12 +33,24 @@ export const TransactionTable = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Super Mercado</td>
-            <td className="withdraw">- R$ 500,00</td>
-            <td>Compras</td>
-            <td>25/02/2022</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td> {transaction.title} </td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category} </td>
+
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.atDate)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
